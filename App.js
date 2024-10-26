346711b7974c1c8c4b60bc7d94ebdaaaf69f8eb1
alvacode <alvaro89_piura@hotmail.com>
2024-10-26 10:20:05 -0500

    context

--- App.js
+++ App.js
@@ -1,34 +1,45 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Home, Login, RifaSuccess, SelectedRifa } from './components';
+import { useState } from 'react';
 
 const Stack = createNativeStackNavigator();
 
 export default function Page() {
-
+  const [isSigned, setIsSigned] = useState(false);
   return (
     <NavigationContainer>
         <Stack.Navigator>
-            <Stack.Screen
-                name='Home'
-                component={Home}
-                options={{headerShown:false}}
-            />
-            <Stack.Screen
-                
-                name='Login'
-                component={Login}
-            />    
-          <Stack.Screen
-              name='SelectedRifa'
-              component={SelectedRifa}
-              options={{title : "Rifa Seleccionada"}}
-          />
-          <Stack.Screen
-              name='RifaSuccess'
-              component={RifaSuccess}
-              options={{headerShown : false}}
-          />
+
+            {
+              isSigned ?
+              <>
+                <Stack.Screen
+                    name='Home'
+                    component={Home}
+                    options={{headerShown:false}}
+                />
+    
+                <Stack.Screen
+                    name='SelectedRifa'
+                    component={SelectedRifa}
+                    options={{title : "Rifa Seleccionada"}}
+                />
+                <Stack.Screen
+                    name='RifaSuccess'
+                    component={RifaSuccess}
+                    options={{headerShown : false}}
+                />
+              </> : 
+              <>
+                <Stack.Screen
+                  name='Login'
+                  component={Login}
+                  options={{headerShown : false}}
+                />  
+              </>
+            }
+
         </Stack.Navigator>
     </NavigationContainer>
   );
