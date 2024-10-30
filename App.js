db5415601c943933165411d63b9c4ce1471fb90d
alvaro <alvaro89_piura@hotmail.com>
2024-10-30 13:32:11 -0500

    cambio de android

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import "react-native-gesture-handler"
+import {enableScreens} from "react-native-screens"
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Home, Login, RifaSuccess, SelectedRifa, TicketDetail, TicketDetailSold, TicketsAvailable, TicketsBooked, TicketsSold, TicketsSoldLast } from './components';
@@ -8,6 +10,7 @@ import { loginPostUser } from './conexion/apiConexion';
 import DrawerNavigator from './DrawerApp';
 import { Alert } from 'react-native';
 
+enableScreens();
 const Stack = createNativeStackNavigator();
 
 export default function Page({ navigation }) {
